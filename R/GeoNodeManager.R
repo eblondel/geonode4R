f0ee5f5df4e250305a5188f81542056b29e343e7
eblondel <emmanuel.blondel1@gmail.com>
2022-11-21 21:11:42 +0100

    fix #1

---
+++ R/GeoNodeManager.R
@@ -0,0 +1,164 @@
+#' GeoNode REST API Manager
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @importFrom openssl base64_encode
+#' @import httr
+#' @import XML
+#' @import keyring
+#' @importFrom readr read_csv
+#' @importFrom readr write_csv
+#' @export
+#' @keywords geonode rest api
+#' @return Object of \code{\link{R6Class}} with methods for communication with
+#' the REST API of a GeoNode instance.
+#' @format \code{\link{R6Class}} object.
+#' 
+#' @examples
+#' \dontrun{
+#'    GeoNodeManager$new("http://localhost:8080/geonode", "admin", "geonode")
+#' }
+#' 
+#' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
+GeoNodeManager <- R6Class("GeoNodeManager",
+   lock_objects = FALSE,
+   
+   private = list(
+     keyring_backend = NULL,
+     keyring_service = NULL,
+     user = NA
+   ),
+   
+   public = list(
+     
+     #' @field verbose.info if geonode4R logs have to be printed
+     verbose.info = FALSE,
+     #' @field verbose.debug if curl logs have to be printed
+     verbose.debug = FALSE,
+     #' @field loggerType the type of logger
+     loggerType = NULL,
+     
+     #'@description Prints a log message
+     #'@param type type of log, "INFO", "WARN", "ERROR"
+     #'@param text text
+     logger = function(type, text){
+       if(self$verbose.info){
+         cat(sprintf("[geonode4R][%s] %s \n", type, text))
+       }
+     },
+     
+     #'@description Prints an INFO log message
+     #'@param text text
+     INFO = function(text){self$logger("INFO", text)},
+     
+     #'@description Prints an WARN log message
+     #'@param text text
+     WARN = function(text){self$logger("WARN", text)},
+     
+     #'@description Prints an ERROR log message
+     #'@param text text
+     ERROR = function(text){self$logger("ERROR", text)},
+     
+     #' @field url the Base url of GeoNode
+     url = NA,
+     
+     #'@description This method is used to instantiate a GeoNodeManager with the \code{url} of the
+     #'    GeoNode and credentials to authenticate (\code{user}/\code{pwd}). 
+     #'    
+     #'    By default, the \code{logger} argument will be set to \code{NULL} (no logger). 
+     #'    This argument accepts two possible values: \code{INFO}: to print only geosapi logs,
+     #'    \code{DEBUG}: to print geosapi and CURL logs.
+     #'    
+     #'    The \code{keyring_backend} can be set to use a different backend for storing 
+     #'    the GeoNode user password with \pkg{keyring} (Default value is 'env').
+     #'@param url url
+     #'@param user user
+     #'@param pwd pwd
+     #'@param logger logger
+     #'@param keyring_backend keyring backend. Default is 'env'
+     initialize = function(url, user, pwd, logger = NULL,
+                           keyring_backend = 'env'){
+       
+       if(!keyring_backend %in% names(keyring:::known_backends)){
+         errMsg <- sprintf("Backend '%s' is not a known keyring backend!", keyring_backend)
+         self$ERROR(errMsg)
+         stop(errMsg)
+       }
+       private$keyring_backend <- keyring:::known_backends[[keyring_backend]]$new()
+       private$keyring_service <- paste0("geonode4R@", url)
+       
+       #logger
+       if(!missing(logger)){
+         if(!is.null(logger)){
+           self$loggerType <- toupper(logger)
+           if(!(self$loggerType %in% c("INFO","DEBUG"))){
+             stop(sprintf("Unknown logger type '%s", logger))
+           }
+           if(self$loggerType == "INFO"){
+             self$verbose.info = TRUE
+           }else if(self$loggerType == "DEBUG"){
+             self$verbose.info = TRUE
+             self$verbose.debug = TRUE
+           }
+         }
+       }
+       
+       #baseUrl
+       if(missing(url)) stop("Please provide the GeoNode base URL")
+       baseUrl = url
+       if(!grepl("/rest", baseUrl)){
+         if(grepl("/$", baseUrl)){
+           baseUrl = paste0(baseUrl, "rest")
+         }else{
+           baseUrl = paste(baseUrl, "rest", sep = "/")
+         }
+       }
+       self$url = baseUrl
+       private$user = user
+       private$keyring_backend$set_with_value(private$keyring_service, username = user, password = pwd)
+
+       self$connect()
+        
+       invisible(self)
+       
+     },
+     
+     #'@description Get URL
+     #'@return the Geoserver URL
+     getUrl = function(){
+       return(self$url)
+     },
+     
+     #'@description Connects to geoServer
+     #'@return \code{TRUE} if connected, raises an error otherwise
+     connect = function(){
+       req <- GeoNodeUtils$GET(
+         url = self$getUrl(), 
+         user = private$user, 
+         pwd = private$keyring_backend$get(service = private$keyring_service, username = private$user), 
+         path = "",
+         verbose = self$verbose.debug
+       )
+       if(status_code(req) == 401){
+         err <- "Impossible to connect to GeoNode: Wrong credentials"
+         self$ERROR(err)
+         stop(err)
+       }
+       if(status_code(req) == 404){
+         err <- "Impossible to connect to GeoNode: Incorrect URL or GeoNode temporarily unavailable"
+         self$ERROR(err)
+         stop(err)
+       }
+       if(status_code(req) != 200){
+         err <- sprintf("Impossible to connect to GeoNode: Unexpected error (status code %s)", status_code(req))
+         self$ERROR(err)
+         stop(err)
+       }else{
+         self$INFO("Successfully connected to GeoNode!")
+       }
+       return(TRUE)
+     }
+     
+   )
+   
+)
---
+++ R/GeoNodeUtils.R
@@ -0,0 +1,156 @@
+#' GeoNode REST API Manager Utils
+#'
+#' @docType class
+#' @export
+#' @keywords geonode rest api
+#' @return Object of \code{\link{R6Class}} with static util methods for communication
+#' with the REST API of a GeoNode instance.
+#' @format \code{\link{R6Class}} object.
+#'
+#' @section Static methods:
+#' \describe{
+#'  \item{\code{getUserAgent()}}{
+#'    This method is used to get the user agent for performing GeoNode API requests.
+#'    Here the user agent will be compound by geonode4R package name and version.
+#'  }
+#'  \item{\code{getUserToken(user, pwd)}}{
+#'    This method is used to get the user authentication token for performing GeoNode
+#'    API requests. Token is given a Base64 encoded string.
+#'  }
+#'  \item{\code{GET(url, user, pwd, path, verbose)}}{
+#'    This method performs a GET request for a given \code{path} to GeoNode REST API
+#'  }
+#'  \item{\code{PUT(url, user, pwd, path, filename, contentType, verbose)}}{
+#'    This method performs a PUT request for a given \code{path} to GeoNode REST API,
+#'    to upload a file of name \code{filename} with given \code{contentType}
+#'  }
+#'  \item{\code{POST(url, user, pwd, path, content, contentType, verbose)}}{
+#'    This method performs a POST request for a given \code{path} to GeoNode REST API,
+#'    to post content of given \code{contentType}
+#'  }
+#'  \item{\code{DELETE(url, user, pwd, path, verbose)}}{
+#'    This method performs a DELETE request for a given GeoServer resource identified
+#'    by a \code{path} in GeoNode REST API
+#'  }
+#'  \item{\code{parseResponseXML(req)}}{
+#'    Convenience method to parse XML response from GeoNode REST API. Although package \pkg{httr}
+#'    suggests the use of \pkg{xml2} package for handling XML, \pkg{geonode4R} still relies
+#'    on the package \pkg{XML}. Response from \pkg{httr} is retrieved as text, and then parsed as
+#'    XML using \code{xmlParse} function.
+#'  }
+#'  \item{\code{getPayloadXML(obj)}}{
+#'    Convenience method to create payload XML to send to GeoNode
+#'  }
+#' }
+#' 
+#' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
+#'
+GeoNodeUtils <- R6Class("GeoNodeUtils")
+
+GeoNodeUtils$getUserAgent <- function(){
+  return(paste("geonode4R", packageVersion("geonode4R"), sep="-"))
+}
+
+GeoNodeUtils$getUserToken <- function(user, pwd){
+  token <- openssl::base64_encode(charToRaw(paste(user, pwd, sep=":")))
+  return(token)
+}
+
+GeoNodeUtils$GET <- function(url, user, pwd, path = "", contentType = "text/xml", verbose = FALSE){
+  if(verbose){
+    req <- with_verbose(GeoNodeUtils$GET(url, user, pwd, path))
+  }else{
+    if(!grepl("^/", path) && path != "") path = paste0("/", path)
+    url <- paste0(url, path) 
+    req <- httr::GET(
+      url = url,
+      add_headers(
+        "User-Agent" = GeoNodeUtils$getUserAgent(),
+        "Authorization" = paste("Basic", GeoNodeUtils$getUserToken(user, pwd)),
+        "Content-Type" = contentType
+      )
+    )
+  }
+  return(req)
+}
+
+GeoNodeUtils$PUT <- function(url, user, pwd, path,
+                        content = NULL, filename = NULL,
+                        contentType, verbose = FALSE){
+  if(verbose){
+    req <- with_verbose(GeoNodeUtils$PUT(url, user, pwd, path, content, filename, contentType))
+  }else{
+    body <- NULL
+    if(missing(content) | is.null(content)){
+      if(missing(filename) | is.null(filename)){
+        stop("The filename must be provided")
+      }
+      body <- httr::upload_file(filename)
+    }else{
+      body <- content
+    }
+    
+    if(!grepl("^/", path)) path = paste0("/", path)
+    url <- paste0(url, path)
+    req <- httr::PUT(
+      url = url,
+      add_headers(
+        "User-Agent" = GeoNodeUtils$getUserAgent(),
+        "Authorization" = paste("Basic", GeoNodeUtils$getUserToken(user, pwd)),
+        "Content-Type" = contentType
+      ),    
+      body = body
+    )
+  }
+  return(req)
+}
+
+GeoNodeUtils$POST <- function(url, user, pwd, path, content, contentType, verbose = FALSE){
+  if(verbose){
+    req <- with_verbose(GeoNodeUtils$POST(url, user, pwd, path, content, contentType))
+  }else{
+    if(!grepl("^/", path)) path = paste0("/", path)
+    url <- paste0(url, path)
+    req <- httr::POST(
+      url = url,
+      add_headers(
+        "User-Agent" = GeoNodeUtils$getUserAgent(),
+        "Authorization" = paste("Basic", GeoNodeUtils$getUserToken(user, pwd)),
+        "Content-Type" = contentType
+      ),    
+      body = content
+    )
+  }
+  return(req)
+}
+
+GeoNodeUtils$DELETE <- function(url, user, pwd, path, verbose = FALSE){
+  if(verbose){
+    req <- with_verbose(GeoNodeUtils$DELETE(url, user, pwd, path))
+  }else{
+    if(!grepl("^/", path)) path = paste0("/", path)
+    url <- paste0(url, path)
+    req <- httr::DELETE(
+      url = url,
+      add_headers(
+        "User-Agent" = GeoNodeUtils$getUserAgent(),
+        "Authorization" = paste("Basic", GeoNodeUtils$getUserToken(user, pwd))
+      )
+    )
+  }
+  return(req)
+}
+
+GeoNodeUtils$parseResponseXML <- function(req){
+  return(xmlParse(content(req, as = "text", encoding = "UTF-8")))
+}
+
+GeoNodeUtils$getPayloadXML <- function(obj){
+  if(!("encode" %in% names(obj))){
+    stop("R6 class with no XML encoder method!")
+  }
+  xml <- obj$encode()
+  xmltext <- as(xml, "character")
+  payload <- gsub("[\r\n ] ", "", xmltext)
+  return(payload)
+}
