b163ab345a99afa77e6ac92b7d2057766c170f00
eblondel <emmanuel.blondel1@gmail.com>
2022-11-21 20:54:38 +0100

    pkg inception

---
+++ R/geonode4R.R
@@ -0,0 +1,13 @@
+#' @name geonode4R
+#' @aliases geonode4R-package
+#' @aliases geonode4R
+#' @docType package
+#' 
+#' @title GeoNode REST API R Interface
+#' @description Provides an R interface to the GeoNode API, allowing to 
+#' upload and publish metadata and data in GeoNode. For more information 
+#' about the GeoNode API, see <https://docs.geonode.org/en/master/devel/api/V2/index.html>
+#' 
+#'@author Emmanuel Blondel \email{emmanuel.blondel1@@gmail.com}
+#'
+NULL
