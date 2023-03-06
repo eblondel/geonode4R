36e47ac9502d8d7f81e641e9c86fc3799e16bdf2
eblondel <emmanuel.blondel1@gmail.com>
2023-03-06 09:07:02 +0100

    setting CI

--- tests/test-all.R
+++ tests/test-all.R
@@ -2,14 +2,14 @@ library(testthat)
 library(geonode4R)
 
 #test environment
-geonodeUrl <- "https://master.demo.geonode.org"
-geonodeUsr <- "user"
-geonodePwd <- "password"
-geonodeLogger <- "INFO"
+geonodeUrl <- "https://stable.demo.geonode.org"
+geonodeUsr <- Sys.getenv("GEONODE_USER")
+geonodePwd <- Sys.getenv("GEONODE_PASSWORD")
+geonodeLogger <- Sys.getenv("GEONODE_LOGGER")
 geonode <- try(GeoNodeManager$new(geonodeUrl, geonodeUsr, geonodePwd, geonodeLogger))
 
 
-if(is(gsman, "GeoNodeManager")){
+if(is(geonode, "GeoNodeManager")){
   cat(sprintf("GeoNode test instance started at %s. Running integration tests...\n", gsUrl))
   test_check("geonode4R")
 }else{
